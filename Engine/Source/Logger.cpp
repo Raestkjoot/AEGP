d850299ffbe401b6cc3c55321311209715b9631e
Raestkjoot <gunnarmagnum@gmail.com>
2024-09-20 22:16:40 +0200

    Added SDL and glm

--- Engine/Source/Logger.cpp
+++ Engine/Source/Logger.cpp
@@ -1,9 +1,12 @@
 #include "Logger.h"
 
 #include <spdlog/spdlog.h>
+#include <glm/glm.hpp>
 
 namespace Logger {
+	glm::vec2 glmVec{ 0.1f, 2.0f };
+
 	void Print(const std::string& str) {
-		spdlog::info(str);
+		spdlog::info("X = {}", glmVec.x);
 	}
 }
