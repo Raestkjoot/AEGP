a978004923bb22cd4cc2a848b499b0bb69884e20
Raestkjoot <gunnarmagnum@gmail.com>
2024-09-24 01:24:01 +0200

    logger - removed static

--- Engine/Source/Logger.h
+++ Engine/Source/Logger.h
@@ -14,36 +14,36 @@ namespace Logger {
 	};
 
 	template <typename... Args>
-	inline static void Print(Level level, std::string message, Args... args) {
+	inline void Print(Level level, std::string message, Args... args) {
 		spdlog::log(static_cast<spdlog::level::level_enum>(level), message, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
-	inline static void Print(std::string message, Args... args) {
+	inline void Print(std::string message, Args... args) {
 		Print(Level::Info, message, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
-	inline static void PrintTrace(std::string message, Args... args) {
+	inline void PrintTrace(std::string message, Args... args) {
 		Print(Level::Trace, message, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
-	inline static void PrintDebug(std::string message, Args... args) {
+	inline void PrintDebug(std::string message, Args... args) {
 		Print(Level::Debug, message, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
-	inline static void PrintWarning(std::string message, Args... args) {
+	inline void PrintWarning(std::string message, Args... args) {
 		Print(Level::Warning, message, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
-	inline static void PrintError(std::string message, Args... args) {
+	inline void PrintError(std::string message, Args... args) {
 		Print(Level::Error, message, std::forward<Args>(args)...);
 	}
 
-	inline static void SetLevel(Level level) {
+	inline void SetLevel(Level level) {
 		spdlog::set_level(static_cast<spdlog::level::level_enum>(level));
 	}
 }
