e5fefdea391c3532e1bea44b54e269dd8f0e1ee5
Gunnar M <gunnarmagnum@gmail.com>
2024-10-26 01:07:53 +0200

    Hello JSON

--- Game/Source/HelloSystem.cpp
+++ Game/Source/HelloSystem.cpp
@@ -6,11 +6,33 @@
 #include "Application.h"
 
 #include <GLFW/glfw3.h>
+#include <nlohmann/json.hpp>
+
+#include <fstream>
 
 void HelloSystem::Start() {
 	Logger::Print("Hello, Start()!");
 	ServiceLocator::GetInputManager()->ListenToKey(GLFW_KEY_F);
 	ServiceLocator::GetInputManager()->ListenToKey(GLFW_KEY_ESCAPE);
+
+	// Read JSON
+	std::ifstream f("Engine/Assets/DefaultTextures.json");
+	nlohmann::json data = nlohmann::json::parse(f);
+	std::string elem0 = data.at("frames").at(0).at("filename").get<std::string>();
+	int elem1;
+	data.at("frames").at(0).at("frame").at("w").get_to(elem1);
+	Logger::Print("File {} has width {}", elem0, elem1);
+
+	// Write JSON
+	auto j = R"(
+	  {
+		"Hello": "World",
+		"isAlive": true,
+		"pi": 3.14159265
+	  }
+	)"_json;
+	std::ofstream o("output.json");
+	o << j.dump(4);
 }
 
 void HelloSystem::Update(float delta) {
