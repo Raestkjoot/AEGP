3cb7cec39ab74f8dc8fbc37a55520a5b3d264356
Gunnar M <gunnarmagnum@gmail.com>
2024-11-18 18:25:59 +0100

    Jump polish
    - jump input cache (pressed & released)
    - coyote time
    - transfer hori velocity to vert by moving against initial horizontal direction while in air
    - variable renaming

--- Game/Source/MoveSystem.cpp
+++ Game/Source/MoveSystem.cpp
@@ -30,7 +30,8 @@ void MoveSystem::Update(float delta) {
 	auto& playerTransform = _registry->get<Transform>(player);
 	auto& playerController = _registry->get<PlayerController>(player);
 
-	HandleInput(playerController.isGrounded);
+	_passedTime += delta;
+	HandleInput();
 
 	if (playerController.isTouchingRight) {
 		if (_velocity.x > 0.0f) {
@@ -43,22 +44,36 @@ void MoveSystem::Update(float delta) {
 		}
 	}
 
+	if (_jumpReleased == false && CanJump(playerController.isGrounded)) {
+		_velocity.y = _jumpPower;
+	}
+
 	if (playerController.isGrounded) {
 		if (_moveDirection != 0.0f) {
-			_velocity.x = glm::clamp(_velocity.x + _moveDirection * _groundMoveAcceleration * delta, -_maxVelocity, _maxVelocity);
+			_velocity.x = glm::clamp(_velocity.x + _moveDirection * _groundAcceleration * delta, -_maxHorizontalSpeed, _maxHorizontalSpeed);
 		} else {
-			_velocity.x -= _velocity.x * _groundStopAcceleration * delta;
+			_velocity.x -= _velocity.x * _groundDeceleration * delta;
 		}
 		
-		if (_jump) {
-			_velocity.y = _jumpPower;
-		} else if (_velocity.y < 0.0f) {
+		if (_velocity.y < 0.0f) {
 			_velocity.y = 0.0f;
 		}
+
+		_wasGroundedLastFrame = true;
 	} else {
-		_velocity.x = glm::clamp(_velocity.x + _moveDirection * _airMoveAcceleration * delta, -_maxVelocity, _maxVelocity);
-		if (_jumpReleased) {
-			_velocity.y -= _jumpReleasedGravity * delta;
+		if (_wasGroundedLastFrame) {
+			_wasGroundedLastFrame = false;
+			if (_velocity.y < 1.0f)
+				_lastGroundedTime = _passedTime;
+		}
+
+		_velocity.x = glm::clamp(_velocity.x + _moveDirection * _airHorizontalAcceleration * delta, -_maxHorizontalSpeed, _maxHorizontalSpeed);
+		if (_velocity.y < 0.0f && _moveDirection * _velocity.x < 0.0f) {
+			_velocity.y += _moveDirection * _airHorizontalAcceleration * delta;
+		}
+
+		if (_jumpReleased || _velocity.y < 0.0f) {
+			_velocity.y -= _gravity * delta;
 		} else {
 			_velocity.y -= _jumpHeldGravity * delta;
 		}
@@ -67,7 +82,7 @@ void MoveSystem::Update(float delta) {
 	playerTransform.position += _velocity * delta;
 }
 
-void MoveSystem::HandleInput(bool isGrounded) {
+void MoveSystem::HandleInput() {
 	if (ServiceLocator::GetInputManager()->GetKeyDown(GLFW_KEY_ESCAPE)) {
 		ServiceLocator::GetApplication()->Quit();
 	}
@@ -83,15 +98,22 @@ void MoveSystem::HandleInput(bool isGrounded) {
 		ServiceLocator::GetInputManager()->GetKey(GLFW_KEY_RIGHT)) {
 		_moveDirection += 1.0f;
 	}
-	if (ServiceLocator::GetInputManager()->GetKey(GLFW_KEY_W) ||
-		ServiceLocator::GetInputManager()->GetKey(GLFW_KEY_UP)) {
-		if (_jumpReleased && isGrounded) {
-			_jump = true;
-			_jumpReleased = false;
-		}
+	if (ServiceLocator::GetInputManager()->GetKeyDown(GLFW_KEY_W) ||
+		ServiceLocator::GetInputManager()->GetKeyDown(GLFW_KEY_UP)) {
+		_jumpReleased = false;
+		_jumpPressedTime = _passedTime;
 	}
 	if (ServiceLocator::GetInputManager()->GetKeyUp(GLFW_KEY_W) ||
 		ServiceLocator::GetInputManager()->GetKeyUp(GLFW_KEY_UP)) {
 		_jumpReleased = true;
+		_jumpReleasedTime = _passedTime;
 	}
+}
+
+bool MoveSystem::CanJump(bool isGrounded) {
+	bool coyote = _passedTime - _lastGroundedTime < _coyoteTime;
+	bool keyPressedInTime = _passedTime - _jumpPressedTime < _jumpCache2 ||
+		_passedTime - _jumpReleasedTime < _jumpCache1;
+
+	return (isGrounded || coyote) && keyPressedInTime;
 }
--- Game/Source/MoveSystem.h
+++ Game/Source/MoveSystem.h
@@ -10,18 +10,36 @@ public:
 	void Update(float delta) override;
 
 private:
-	void HandleInput(bool isGrounded);
+	void HandleInput();
+	bool CanJump(bool isGrounded);
 
 	glm::vec2 _velocity;
 	float _moveDirection;
 	bool _jump;
 	bool _jumpReleased = true;
+	float _jumpPressedTime;
+	float _jumpReleasedTime;
+	float _passedTime = 0.0f;
+	float _lastGroundedTime = 0.0f;
+	bool _wasGroundedLastFrame;
 
-	float _maxVelocity = 2.0f;
-	float _groundMoveAcceleration = 28.0f;
-	float _groundStopAcceleration = 20.0f;
-	float _airMoveAcceleration = 16.0f;
+#pragma region tweak values
+	float _maxHorizontalSpeed = 2.0f;
+	float _groundAcceleration = 28.0f;
+	float _groundDeceleration = 20.0f;
+	float _airHorizontalAcceleration = 16.0f;
+	// Air vert acc is applied when the player moves against the initial horizontal input,
+	// giving a feel of converting hori velocity to vert velocity.
+	float _airVerticalAcceleration = 8.0f;
 	float _jumpPower = 6.0f;
+	// gravity after a jump depends on whether jump is being held down or not.
+	float _gravity = 25.0f;
 	float _jumpHeldGravity = 10.0f;
-	float _jumpReleasedGravity = 25.0f;
+	// Store jump input for this long, after player releases jump button
+	float _jumpCache1 = 0.05f;
+	// Store jump input for this long, while player is holding jump button
+	float _jumpCache2 = 0.4f;
+	// Let player still jump for this time, after running off an edge
+	float _coyoteTime = 0.1f;
+#pragma endregion tweak values
 };
